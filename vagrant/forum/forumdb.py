9f9b4ca4534805a81d354bedb0aeab464389d79b
leenpaws <s.chhaya@gmail.com>
2016-04-21 05:00:48 -0400

    Sql Module

--- vagrant/forum/forumdb.py
+++ vagrant/forum/forumdb.py
@@ -2,30 +2,40 @@
 # Database access functions for the web forum.
 # 
 
-import time
 
-## Database connection
-DB = []
+import psycopg2
 
-## Get posts from database.
+
+# Database connection
+
+# Get posts from database.
 def GetAllPosts():
-    '''Get all the posts from the database, sorted with the newest first.
+    db = psycopg2.connect("dbname=forum")
+    c = db.cursor()
 
+    '''Get all the posts from the database, sorted with the newest first.
     Returns:
       A list of dictionaries, where each dictionary has a 'content' key
       pointing to the post content, and 'time' key pointing to the time
       it was posted.
     '''
-    posts = [{'content': str(row[1]), 'time': str(row[0])} for row in DB]
-    posts.sort(key=lambda row: row['time'], reverse=True)
+    c.execute("SELECT time, content FROM posts ORDER BY time DESC")
+    posts = ({'content': str(row[1]), 'time': str(row[0])}
+             for row in c.fetchall())
+    db.close()
     return posts
 
-## Add a post to the database.
-def AddPost(content):
-    '''Add a new post to the database.
 
-    Args:
+# Add a post to the database.
+def AddPost(content):
+    db = psycopg2.connect("dbname=forum")
+
+    c = db.cursor()
+    '''Add a new post to the database.'''
+    '''   Args:
       content: The text content of the new post.
     '''
-    t = time.strftime('%c', time.localtime())
-    DB.append((t, content))
+
+    c.execute("INSERT INTO posts (content) VALUES ('%s')", (content,))
+    db.commit()
+    db.close()
